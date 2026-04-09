fbc6e4df706d5163ff52556280b36d31f1dddfb4
sudipto818 <sudiptoghosh096@gmail.com>
2026-04-09 12:54:32 +0530

    update impl.hpp of spsc_unbounded

--- include/tsq/lockfree_spsc_unbounded/impl.hpp
+++ include/tsq/lockfree_spsc_unbounded/impl.hpp
@@ -9,7 +9,7 @@ using queue = tsfqueue::__impl::lockfree_spsc_unbounded<T>;
 template <typename T> void queue<T>::push(T value) {
     node* temp = tail.load(std::memory_order_relaxed);
     temp->data = std::move(value);
-    new_tail = new node();
+    node* new_tail = new node();
     new_tail->next.store(nullptr,std::memory_order_relaxed);
     temp->next.store(new_tail,std::memory_order_release);
     tail.store(new_tail,std::memory_order_relaxed);
@@ -26,11 +26,31 @@ template <typename T> bool queue<T>::try_pop(T &value) {
     return true;
 }
 
-template <typename T> void queue<T>::wait_and_pop(T &value) {}
+template <typename T> void queue<T>::wait_and_pop(T &value) {
+    while(!try_pop(value)){
+        std::this_thread::yield(); //Yield execution to allow other threads to run 
+    } 
+}
 
-template <typename T> bool queue<T>::peek(T &value) {}
+template <typename T> bool queue<T>::peek(T &value) {//Not atomic as a whole, so it is not a proper peek operation in general except spsc. 
+    // Lets' say the value of the first element was read by a thread and then the first element got popped from the
+    // queue by some other thread. So then again the initial thread which did the peek will return the value of the new front element.
+    // But it is safe in SPSC as only consumer thread has access to the front element, so the implementation is correct.
+    if (empty()) {
+        return false;
+    }
+    value=head->data;
+    return true;
 
-template <typename T> bool queue<T>::empty(void) {}
+}
+
+template <typename T> bool queue<T>::empty(void) {
+    node* next = head->next.load(std::memory_order_acquire);
+    if(next==nullptr){
+        return true;
+    }
+    return false;
+}
 
 #endif
 
