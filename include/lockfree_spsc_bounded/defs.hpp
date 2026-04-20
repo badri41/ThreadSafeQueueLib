5da66b1706047758bac491e11a5f80c5ae2a97e5
badri41 <badribishaldas3000@gmail.com>
2026-04-20 20:56:17 +0530

    Add static asserts and made the byte array

--- include/lockfree_spsc_bounded/defs.hpp
+++ include/lockfree_spsc_bounded/defs.hpp
@@ -9,91 +9,89 @@
 #include <type_traits>
 namespace tsfqueue::impl {
 template <typename T, size_t Capacity> class lockfree_spsc_bounded {
-  static_assert(std::is_object_v<T>, "T must be an object type");
-  static_assert(std::is_destructible_v<T>, "T must be destructible");
+	static_assert(std::is_object_v<T>, "T must be an object type");
+	static_assert(std::is_destructible_v<T>, "T must be destructible");
+	static_assert(std::is_copy_constructible_v<T>, "T must be copy constructible");
+	static_assert(std::is_move_constructible_v<T>, "T must be move constructible");
 
-  // For the implementation, we first take the size of the bounded queue from
-  // user inside the templates so that we can do compile time memory allocation.
-  // We have two atomic pointer, head and tail, tail for pushing the element and
-  // head for popping. We also add check tail == head for empty which means one
-  // redundant element during allocation. We keep head_cache and tail_cache as
-  // cached copies to have a cache efficient code (discuss with me for details).
-  // All the data members are cache aligned to prevent cache-line bouncing.
-  // The user is provided with both set of functions : try_pop() and try_push()
-  // for a wait-free code And wait_and_pop() and wait_and_push() for a lock-less
-  // code but not wait-free variant. Thus, the user is given a choice to choose
-  // among the preferred endpoints as per use case.
-private:
-  // Add the private members :
-  // alignas(cache_line_size) std::atomic<size_t> head;
-  // alignas(cache_line_size) size_t tail_cache;
-  // alignas(cache_line_size) std::atomic<size_t> tail;
-  // alignas(cache_line_size) size_t head_cache;
-  // static constexpr size_t capacity = Capacity + 1;
-  // alignas(cache_line_size) T arr[capacity];
-  // aligned the start of the array too
-  // Description of private members :
-  // 1. std::atomic<size_t> head is the atomic head pointer
-  // 2. std::atomic<size_t> tail is the atomic tail pointer
-  // 3. size_t head_cache is the cached head pointer
-  // 4. size_t tail_cache is the cached tail pointer
-  // 5. T arr[] compile time allocated array
-  // Cache align 1-5.
-  // 6. static constexpr size_t capcity to store the capcity for operations in
-  // functions Why static ?? Why constexpr ?? [Reason this]
-  alignas(cache_line_size) std::atomic<size_t> head{0};
-  alignas(cache_line_size) size_t tail_cache{0};
-  alignas(cache_line_size) std::atomic<size_t> tail{0};
-  alignas(cache_line_size) size_t head_cache{0};
-  static constexpr size_t capacity = Capacity + 1; 
-  alignas(cache_line_size) alignas(T) std::byte arr[capacity * sizeof(T)]{};
-	
-  // Make these functions which expose raw pointers private for safety... 
-  T *slot_ptr(size_t idx) noexcept {
-	return std::launder(
-		reinterpret_cast<T *>(arr + (idx * sizeof(T))));
-  }
-
-  const T *slot_ptr(size_t idx) const noexcept {
-	return std::launder(
-		reinterpret_cast<const T *>(arr + (idx * sizeof(T))));
-  }
-
+	// For the implementation, we first take the size of the bounded queue from
+	// user inside the templates so that we can do compile time memory allocation.
+	// We have two atomic pointer, head and tail, tail for pushing the element and
+	// head for popping. We also add check tail == head for empty which means one
+	// redundant element during allocation. We keep head_cache and tail_cache as
+	// cached copies to have a cache efficient code (discuss with me for details).
+	// All the data members are cache aligned to prevent cache-line bouncing.
+	// The user is provided with both set of functions : try_pop() and try_push()
+	// for a wait-free code And wait_and_pop() and wait_and_push() for a lock-less
+	// code but not wait-free variant. Thus, the user is given a choice to choose
+	// among the preferred endpoints as per use case.
 public:
-  // Public Member functions :
-  // Add appropriate constructors and destructors -> Add here only
-  // 1. void wait_and_push(value) : Busy wait until element is pushed
-  // 2. bool try_push(value) : Try to push if not full else leave (returns false
-  // if could not push else true)
-  // 3. void wait_and_pop(value ref) : Busy wait until we have atmost 1 elt and
-  // then pop it and store in reference
-  // 4. bool try_pop(value ref) : Try to pop and return false if failed bool
-  // 5. empty(void) : Checks if the queue is empty and return bool
-  // 6. bool peek(value ref) : Peek the top of the queue.
-  // Will work only in SPSC/MPSC why ?? [Reason this]
-  // 8. Add emplace_back using perfect forwarding and variadic templates (you
-  // can use this in push then)
-  // 9. Add size() function
-  // 10. Any more suggestions ??
-  // can make the functions peek , empty and size const
-  // 11. Why no shared_ptr ?? [Reason this]
-  lockfree_spsc_bounded() = default;
-  lockfree_spsc_bounded(const lockfree_spsc_bounded &) = delete;
-  lockfree_spsc_bounded &operator=(const lockfree_spsc_bounded &) = delete;
-  lockfree_spsc_bounded(lockfree_spsc_bounded &&) = delete;
-  lockfree_spsc_bounded &operator=(lockfree_spsc_bounded &&) = delete;
-  ~lockfree_spsc_bounded();
+	// Public Member functions :
+	// Add appropriate constructors and destructors -> Add here only
+	// 1. void wait_and_push(value) : Busy wait until element is pushed
+	// 2. bool try_push(value) : Try to push if not full else leave (returns false
+	// if could not push else true)
+	// 3. void wait_and_pop(value ref) : Busy wait until we have atmost 1 elt and
+	// then pop it and store in reference
+	// 4. bool try_pop(value ref) : Try to pop and return false if failed bool
+	// 5. empty(void) : Checks if the queue is empty and return bool
+	// 6. bool peek(value ref) : Peek the top of the queue.
+	// Will work only in SPSC/MPSC why ?? [Reason this]
+	// 8. Add emplace_back using perfect forwarding and variadic templates (you
+	// can use this in push then)
+	// 9. Add size() function
+	// 10. Any more suggestions ??
+	// can make the functions peek , empty and size const
+	// 11. Why no shared_ptr ?? [Reason this]
+	lockfree_spsc_bounded() = default;
+	lockfree_spsc_bounded(const lockfree_spsc_bounded &) = delete;
+	lockfree_spsc_bounded &operator=(const lockfree_spsc_bounded &) = delete;
+	lockfree_spsc_bounded(lockfree_spsc_bounded &&) = delete; // move constructor deleted...
+	lockfree_spsc_bounded &operator=(lockfree_spsc_bounded &&) = delete; // move assignment operator deleted...
+	~lockfree_spsc_bounded();
 
-  void wait_and_push(T value);
-  bool try_push(T value);
-  void wait_and_pop(T &value);
-  bool try_pop(T &value);
-  bool peek(T &value);
+	void wait_and_push(T value);
+	bool try_push(T value);
+	void wait_and_pop(T &value);
+	bool try_pop(T &value);
+	bool peek(T &value);
 
-  template <typename... Args> bool emplace_back(Args &&...args); // emplace using variadic templates and perfect forwarding
+	template <typename... Args> bool emplace_back(Args &&...args); // emplace using variadic templates and perfect forwarding
 
-  bool empty() const;
-  size_t size() const;
+	bool empty() const;
+	size_t size() const;
+
+private:
+	// Add the private members :
+	// alignas(cache_line_size) std::atomic<size_t> head;
+	// alignas(cache_line_size) size_t tail_cache;
+	// alignas(cache_line_size) std::atomic<size_t> tail;
+	// alignas(cache_line_size) size_t head_cache;
+	// static constexpr size_t capacity = Capacity + 1;
+	// alignas(cache_line_size) T arr[capacity];
+	// aligned the start of the array too
+	// Description of private members :
+	// 1. std::atomic<size_t> head is the atomic head pointer
+	// 2. std::atomic<size_t> tail is the atomic tail pointer
+	// 3. size_t head_cache is the cached head pointer
+	// 4. size_t tail_cache is the cached tail pointer
+	// 5. T arr[] compile time allocated array
+	// Cache align 1-5.
+	// 6. static constexpr size_t capcity to store the capcity for operations in
+	// functions Why static ?? Why constexpr ?? [Reason this]
+	alignas(cache_line_size) std::atomic<size_t> head{0};
+	alignas(cache_line_size) size_t tail_cache{0};
+	alignas(cache_line_size) std::atomic<size_t> tail{0};
+	alignas(cache_line_size) size_t head_cache{0};
+	static constexpr size_t capacity = Capacity + 1; 
+
+	// Use byte array to store the data to avoid unnecessary construction and destruction ... 
+	alignas(cache_line_size) alignas(T) std::byte arr[capacity * sizeof(T)]{};
+		
+	// Make these functions which expose raw pointers private for safety... 
+	T *slot_ptr(size_t idx) noexcept {
+		return std::launder(reinterpret_cast<T *>(arr + (idx * sizeof(T))));
+	}
 };
 } // namespace tsfqueue::impl
 #endif
--- include/lockfree_spsc_bounded/impl.hpp
+++ include/lockfree_spsc_bounded/impl.hpp
@@ -9,122 +9,123 @@ namespace tsfqueue::impl {
 
 template <typename T, size_t Capacity>
 void lockfree_spsc_bounded<T, Capacity>::wait_and_push(T value) {
-  size_t cur_tail = tail.load(std::memory_order_relaxed);
-  //We should change the tail.load(acquire) to tail.load(relaxed) since only the producer thread has 
-  // exclusive write access to the tail variable and a single thread always agrees upon the order of modification of an atomic variable.
-  //tail.load(relaxed) is less costly.
-  size_t next_tail = (cur_tail + 1) % capacity;
-  // size_t curr_head = head.load(std::memory_order_acquire);
-  while (next_tail == head_cache) {
-	head_cache = head.load(std::memory_order_acquire); // busy wait
-  }
+	size_t cur_tail = tail.load(std::memory_order_relaxed);
+	//We should change the tail.load(acquire) to tail.load(relaxed) since only the producer thread has 
+	// exclusive write access to the tail variable and a single thread always agrees upon the order of modification of an atomic variable.
+	//tail.load(relaxed) is less costly.
+	size_t next_tail = (cur_tail + 1) % capacity;
+	// size_t curr_head = head.load(std::memory_order_acquire);
+	while (next_tail == head_cache) {
+		head_cache = head.load(std::memory_order_acquire); // busy wait
+	}
 
-  std::construct_at(slot_ptr(cur_tail), std::move(value));
-  // tail_cache = next_tail;
-  tail.store(next_tail, std::memory_order_release);
+	std::construct_at(slot_ptr(cur_tail), std::move(value));
+	// tail_cache = next_tail;
+	tail.store(next_tail, std::memory_order_release);
 }
 
 template <typename T, size_t Capacity>
 bool lockfree_spsc_bounded<T, Capacity>::try_push(T value) {
-  return emplace_back(std::move(value));
+  	return emplace_back(std::move(value));
 }
 
 template <typename T, size_t Capacity>
 bool lockfree_spsc_bounded<T, Capacity>::try_pop(T &value) {
-  // cur_tail = tail.load(std::memory_order_acquire);
-  size_t cur_head = head.load(std::memory_order_relaxed);
-  //We should change the head.load(acquire) to head.load(relaxed) since only the producer thread has 
-  // exclusive write access to the head variable and a single thread always agrees upon the order of modification of an atomic variable.
-  //head.load(relaxed) is less costly.
-  if (tail_cache == cur_head) {
-	tail_cache = tail.load(std::memory_order_acquire);
-	if (tail_cache == cur_head)
-	  return false;
-  }
+	// cur_tail = tail.load(std::memory_order_acquire);
+	size_t cur_head = head.load(std::memory_order_relaxed);
+	//We should change the head.load(acquire) to head.load(relaxed) since only the producer thread has 
+	// exclusive write access to the head variable and a single thread always agrees upon the order of modification of an atomic variable.
+	//head.load(relaxed) is less costly.
+	if (tail_cache == cur_head) {
+		tail_cache = tail.load(std::memory_order_acquire);
+		if (tail_cache == cur_head)
+		return false;
+	}
 
-  T *slot = slot_ptr(cur_head);
-  value = std::move(*slot);
-  std::destroy_at(slot);
-  // head_cache = (cur_head + 1) % capacity;
-  head.store((cur_head + 1) % capacity, std::memory_order_release);
-  return true;
+	T *slot = slot_ptr(cur_head);
+	value = std::move(*slot);
+	std::destroy_at(slot);
+	// head_cache = (cur_head + 1) % capacity;
+	head.store((cur_head + 1) % capacity, std::memory_order_release);
+	return true;
 }
 
 template <typename T, size_t Capacity>
 void lockfree_spsc_bounded<T, Capacity>::wait_and_pop(T &value) {
-  size_t cur_head = head.load(std::memory_order_relaxed);
-  //We should change the head.load(acquire) to head.load(relaxed) since only the producer thread has 
-  // exclusive write access to the head variable and a single thread always agrees upon the order of modification of an atomic variable.
-  //head.load(relaxed) is less costly.
-  while (tail_cache == cur_head) {
-	tail_cache = tail.load(std::memory_order_acquire); // busy wait
-  }
+	size_t cur_head = head.load(std::memory_order_relaxed);
+	//We should change the head.load(acquire) to head.load(relaxed) since only the producer thread has 
+	// exclusive write access to the head variable and a single thread always agrees upon the order of modification of an atomic variable.
+	//head.load(relaxed) is less costly.
+	while (tail_cache == cur_head) {
+		tail_cache = tail.load(std::memory_order_acquire); // busy wait
+	}
 
-  T *slot = slot_ptr(cur_head);
-  value = std::move(*slot);
-  std::destroy_at(slot);
-  // head_cache = (cur_head + 1) % capacity;
-  head.store((cur_head + 1) % capacity, std::memory_order_release);
+	T *slot = slot_ptr(cur_head);
+	value = std::move(*slot);
+	std::destroy_at(slot);
+	// head_cache = (cur_head + 1) % capacity;
+	head.store((cur_head + 1) % capacity, std::memory_order_release);
 }
 
 template <typename T, size_t Capacity>
 bool lockfree_spsc_bounded<T, Capacity>::peek(T &value) {
-  size_t cur_head = head.load(std::memory_order_acquire);
-  if (cur_head == tail_cache) {
-	tail_cache = tail.load(std::memory_order_acquire);
+	size_t cur_head = head.load(std::memory_order_acquire);
 	if (cur_head == tail_cache) {
-	  return false;
+		tail_cache = tail.load(std::memory_order_acquire);
+		if (cur_head == tail_cache) {
+		return false;
+		}
 	}
-  }
-  value = *slot_ptr(cur_head);
-  return true;
+	value = *slot_ptr(cur_head);
+	return true;
 }
 
 template <typename T, size_t Capacity>
 template <typename... Args>
 bool lockfree_spsc_bounded<T, Capacity>::emplace_back(Args &&...args) {
-  size_t cur_tail = tail.load(std::memory_order_relaxed);
-  //We should change the tail.load(acquire) to tail.load(relaxed) since only the producer thread has 
-  // exclusive write access to the tail variable and a single thread always agrees upon the order of modification of an atomic variable.
-  //tail.load(relaxed) is less costly.
-  size_t next_tail = (cur_tail + 1) % capacity;
-  if (next_tail == head_cache) {
-	head_cache = head.load(std::memory_order_acquire);
+	size_t cur_tail = tail.load(std::memory_order_relaxed);
+	//We should change the tail.load(acquire) to tail.load(relaxed) since only the producer thread has 
+	// exclusive write access to the tail variable and a single thread always agrees upon the order of modification of an atomic variable.
+	//tail.load(relaxed) is less costly.
+	size_t next_tail = (cur_tail + 1) % capacity;
 	if (next_tail == head_cache) {
-	  return false;
+		head_cache = head.load(std::memory_order_acquire);
+		if (next_tail == head_cache) {
+		return false;
+		}
 	}
-  }
-  std::construct_at(slot_ptr(cur_tail), std::forward<Args>(args)...);
-  // tail_cache = next_tail;
-  tail.store(next_tail, std::memory_order_release);
-  return true;
+	std::construct_at(slot_ptr(cur_tail), std::forward<Args>(args)...);
+	// tail_cache = next_tail;
+	tail.store(next_tail, std::memory_order_release);
+	return true;
 }
 
 template <typename T, size_t Capacity>
 bool lockfree_spsc_bounded<T, Capacity>::empty() const {
-  return head.load(std::memory_order_relaxed) ==
-		 tail.load(std::memory_order_relaxed);
-  // since queue is very frequently modified
+	return head.load(std::memory_order_relaxed) ==
+			tail.load(std::memory_order_relaxed);
+	// since queue is very frequently modified
 }
 
 template <typename T, size_t Capacity>
 size_t lockfree_spsc_bounded<T, Capacity>::size() const {
-  return (tail.load(std::memory_order_relaxed) -
-		  head.load(std::memory_order_relaxed) + capacity) %
-		 capacity;
-  // again, since size is very frequently changing.
+	return (tail.load(std::memory_order_relaxed) -
+			head.load(std::memory_order_relaxed) + capacity) %
+			capacity;
+	// again, since size is very frequently changing.
 }
 
 template <typename T, size_t Capacity>
 lockfree_spsc_bounded<T, Capacity>::~lockfree_spsc_bounded() {
-  size_t cur_head = head.load(std::memory_order_relaxed);
-  const size_t cur_tail = tail.load(std::memory_order_relaxed);
+	size_t cur_head = head.load(std::memory_order_relaxed);
+	slot_ptr size_t cur_tail = tail.load(std::memory_order_relaxed);
 
-  while (cur_head != cur_tail) {
-    std::destroy_at(slot_ptr(cur_head));
-    cur_head = (cur_head + 1) % capacity;
-	}
+	while (cur_head != cur_tail) {
+		std::destroy_at(slot_ptr(cur_head));
+		cur_head = (cur_head + 1) % capacity;
+		}
 }
+
 } // namespace tsfqueue::impl
 
 #endif
